0cd603c0c983b322b4e7ae46afc33506e97eab0c
Agustin Picotto <agustin.picotto@gmail.com>
2022-04-23 14:28:39 -0300

    BaSP-2022: Login js files almost complete

---
+++ Semana-06/javascript/login.js
@@ -0,0 +1,44 @@
+window.onload = function() {
+    var lbEmail = document.getElementById('lb-login-email');
+    var lbPass = document.getElementById('lb-login-pass');
+    lbEmail.style.display = "none";
+    lbPass.style.display = "none";
+    var comprobation1, comprobation2, largePass;
+    var emailLogin = document.getElementById('email-login');
+    emailLogin.addEventListener('focus',function(){
+    lbEmail.style.display = "none";
+    })
+    emailLogin.addEventListener('blur',function(){
+        comprobation1 = emailLogin.value.indexOf("@");  
+        if (comprobation1===-1){
+            lbEmail.style.display = "flex";
+        } else {
+            lbEmail.style.display = "none";
+            comprobation1=2;
+        }
+    })
+    var passLogin = document.getElementById('pass-login');
+        passLogin.addEventListener('focus',function(){
+        lbPass.style.display = "none";
+    })
+    passLogin.addEventListener('blur',function(){
+        largePass = passLogin.value;
+        if (largePass.length<7){
+            comprobation2=-1;
+        }
+        if (comprobation2===-1){
+            lbPass.style.display = "flex";
+        } else {
+            lbPass.style.display = "none";
+            comprobation2=2;
+        }
+    })
+    var btLogin = document.getElementById('button-login');
+    btLogin.addEventListener('click', function(){
+        if(comprobation1===2 && comprobation2===2){
+            window.alert("Your are login");
+        } else {
+            window.alert("Please review the email and password inputs");
+        }
+    })
+}
